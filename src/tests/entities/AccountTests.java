68695e5e09978f7bcf58a69885bde54788192078
Rafael de Luca <deluca1712@gmail.com>
2022-08-16 11:21:44 -0300

    Teste metodo de saque com e sem saldo suficiente

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -53,14 +53,37 @@ public class AccountTests {
 	}
 	
 	@Test
-	public void interestEarnedWhenReceiveInterest () {
+	public void withdrawShouldDecreaseBalanceWhenThereIsSufficienteBalance() {
+		
+		Account acc = AccountFactory.createAccount(1000.0);
+		acc.withdraw(800.0);
+		//sacou 800 de um conta com saldo 1000, tem que sobre 200
+		
+		Assertions.assertEquals(200,acc.getBalance());
+	}
+	
+	//para testar exceção tem que capturar a classe do tipo da exceção e usar uma
+	//expressão lambda no argumento do executável
+	
+	@Test
+	public void withdrawShouldThrowExceptionWhenThereIsInsufficienteBalance() {
+		//se tentar sacar um valor maior que o saldo deve lançar a exceção.
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Account conta = AccountFactory.createAccount(900.0);
+			conta.withdraw(900.50);
+			
+		});
+	}
+	
+	
+	@Test
+	public void interestShouldEarnedWhenReceiveInterest () {
 		
 		double expectedValue = 1500 + 1500 * 0.01;
 		double initialBalance = 1500;
 		Account conta = AccountFactory.createAccount(initialBalance);	
 		conta.interestEarned();
-		Assertions.assertEquals(expectedValue, conta.getBalance());
-		
+		Assertions.assertEquals(expectedValue, conta.getBalance());		
 	}
 	
 
